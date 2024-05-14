8f17ce80545187bb9d3968d9de3a81e7c2320d01
KristofPhillips95 <kristofphillips@gmail.com>
2024-05-14 11:14:00 +0200

    Small updates

--- FullScript_Storage_heavy_2.jl
+++ FullScript_Storage_heavy_2.jl
@@ -19,10 +19,12 @@ types = ["NTC","TCS"]
 stepsizes = [100]
 target_caps_for_curves = ["no_fix"]
 trans_caps_others = ["S"]
-geo_scopes = [["BE00", "DE00","LUG1","FR00","UK00","NL00"]]
+#geo_scopes = [["BE00", "DE00","LUG1","FR00","UK00","NL00"]]
+geo_scopes = ["All"]
 
 #Start looping over desired global parameters: 
-run_name = "storage_heavy_red_geo_fsoc_$(gpd["country"])_$(gpd["endtime"])_reformed_2"
+#run_name = "storage_heavy_red_geo_fsoc_$(gpd["country"])_$(gpd["endtime"])_reformed_2"
+run_name = "storage_heavy_fsoc_$(gpd["country"])_$(gpd["endtime"])_reformed_2"
 results_path = joinpath("Results","InvestmentModelResults_2","$(run_name).csv")
 # m = Model(optimizer_with_attributes(Gurobi.Optimizer))
 # row = full_build_and_optimize_investment_model(m,global_param_dict = gpd)
--- FullScript_Storage_heavy_3.jl
+++ FullScript_Storage_heavy_3.jl
@@ -19,10 +19,13 @@ types = ["TCS"]
 stepsizes = [100]
 target_caps_for_curves = ["TYNDP","endo_invest","0"]
 trans_caps_others = ["S"]
-geo_scopes = [["BE00", "DE00","LUG1","FR00","UK00","NL00"]]
+#geo_scopes = [["BE00", "DE00","LUG1","FR00","UK00","NL00"]]
+geo_scopes = ["All"]
 
 #Start looping over desired global parameters: 
-run_name = "storage_heavy_red_geo_fsoc_$(gpd["country"])_$(gpd["endtime"])_reformed_3"
+#run_name = "storage_heavy_red_geo_fsoc_$(gpd["country"])_$(gpd["endtime"])_reformed_3"
+run_name = "storage_heavy_fsoc_$(gpd["country"])_$(gpd["endtime"])_reformulated_3"
+
 results_path = joinpath("Results","InvestmentModelResults_2","$(run_name).csv")
 # m = Model(optimizer_with_attributes(Gurobi.Optimizer))
 # row = full_build_and_optimize_investment_model(m,global_param_dict = gpd)
